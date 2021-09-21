848923926f6191dea22c55e7e925ed03ae960b9a
Syed Aamish <syedaamish9@gmail.com>
2021-09-21 16:33:55 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,9 @@
+exports.TOKEN = 'BOT TOKEN HERE';
+
+exports.PREFIX = 'BOT PREFIX HERE';
+
+exports.news_API = 'NEWS API HERE';
+
+exports.giphy_API = 'GIPHY API HERE';
+
+exports.AME_API = 'AME API HERE';
